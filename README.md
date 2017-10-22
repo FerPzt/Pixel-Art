1eb4ff063ab6be387a63b45e35ad839bcc257485
Fernanda Pizzolatto <fer.pzt@gmail.com>
2017-10-22 11:24:09 -0700

    Add comments to design.js and updated readme.md

--- README.md
+++ README.md
@@ -1,14 +1,5 @@
 # Pixel Art Maker Project
 
-## Table of Contents
+This is a project from Udacity Front End Nanodegree, for studying purposes.
 
-* [Instructions](#instructions)
-* [Contributing](#contributing)
-
-## Instructions
-
-To use the Pixel Art Maker, you need to open index.html and set the size of the grid (height and width), then select the color in the color picker. After that, you are good to start your pixel art.
-
-## Contributing
-
-Starter code by Udacity, desgins.js implemented by Fernanda.
+With the Pixel Art Maker, you can create a table, select any color from color picker and make your own pixel arts :)
--- designs.js
+++ designs.js
@@ -1,32 +1,42 @@
+//Set the color for the box from color picker
 function colorBox(box) {
   let colour = $('#colorPicker').val();
-  $(box).css('background-color', colour);
+  box.style.backgroundColor = colour;
 }
 
+//Create table based on width and height input
 function makeGrid(width, height) {
   let pixelCanvas, grid;
   pixelCanvas = $('#pixel_canvas');
 
+  //Check if table already has content
   if (pixelCanvas.children().length) {
+    //if it does, empty to add new content
     pixelCanvas.empty();
   }
 
+  //Iterating over width and height to create boxes
   for (let i = 0; i < height; i++) {
     grid += "<tr>";
     for (let j = 0; j < width; j++) {
+      //Assigning the click event for each box
       grid += "<td onclick='colorBox(this)'></td>"
     }
     grid += "</tr>";
   }
+
   pixelCanvas.html(grid);
   pixelCanvas.css('background-color', '#ffffff');
 };
 
 $(document).ready(function(){
-  $('#submit_button').click(function(evt){
+  //Implementing custom submit for the form
+  $('#sizePicker').submit(function(event){
+    //Get the width and height values from user input
     const height = $('#input_height').val();
     const width = $('#input_width').val();
-    evt.preventDefault();
+    //Prevent form to be submitted
+    event.preventDefault();
     makeGrid(width, height);
-  });
+  })
 });
